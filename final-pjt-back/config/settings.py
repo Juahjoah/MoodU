a1a334d3fa63c68d5efeb9372d946ed846ee10a7
yeg7481 <yeg7481@icloud.com>
2023-05-17 11:50:19 +0900

    feat: user model 생성

--- final-pjt-back/config/settings.py
+++ final-pjt-back/config/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
