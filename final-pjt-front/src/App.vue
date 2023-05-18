9a62079d2dc785f0e9a67e9728547ecf20178741
Juahjoah <07juah21@gmail.com>
2023-05-18 14:26:14 +0900

    feat: LoginView 추가

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -3,8 +3,8 @@
     <nav>
       <router-link to="/">Movie</router-link> |
       <router-link to="/community">Community</router-link> |
-      <router-link to="/signup">Sign up</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link :to="{ name: 'SignUpView' }">Sign up page</router-link> |
+      <router-link :to="{ name: 'LoginView' }">Login page</router-link>
     </nav>
     <router-view />
   </div>
--- final-pjt-front/src/views/LoginView.vue
+++ final-pjt-front/src/views/LoginView.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="login">
     <h1>Login</h1>
+    <form @submit.prevent="login">
+      <label for="userid">ID: </label>
+      <input type="text" id="userid" v-model="userid" />
+      <br />
+      <label for="password">PW: </label>
+      <input type="password" id="password" v-model="password" />
+      <br />
+
+      <input type="submit" value="logIn" />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: "LoginView",
+  data() {
+    return {
+      userid: null,
+      password: null,
+    };
+  },
+  methods: {
+    login() {
+      const userid = this.userid;
+      const password = this.password;
+
+      const payload = {
+        userid,
+        password,
+      };
+
+      this.$store.dispatch("login", payload);
+    },
+  },
 };
 </script>
 
