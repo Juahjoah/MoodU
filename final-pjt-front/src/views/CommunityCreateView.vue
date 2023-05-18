a7b722ddb6dfbc79dff3ab01b63514c03988f652
Juahjoah <07juah21@gmail.com>
2023-05-18 17:00:20 +0900

    feat: CommunityCraeteView create & 기본 세팅

--- final-pjt-front/src/views/CommunityCreateView.vue
+++ final-pjt-front/src/views/CommunityCreateView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="create"></div>
+  <div class="create">
+    <h1>커뮤니티 게시글 작성</h1>
+    <form @submit.prevent="createCommunity"></form>
+    <label for="title"> 제목: </label>
+    <input type="text" id="title" v-model.trim="title" />
+  </div>
 </template>
 
 <script>
