5bdff500092457d9ebcfcb6d17d7607d7b4442a5
better-gyeom <better.gyeom@gmail.com>
2023-05-19 08:53:18 +0900

    feat: jwt로 로그인, 회원가입 기능 구현

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,15 +1,45 @@
-from django.shortcuts import render
-import requests
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import User
+from .serializers import UserSerializer
+import jwt
 
-# Create your views here.
 
-def get_movies_data():
+@api_view(['POST'])
+def signup(request):
+    # 아이디, 비밀번호, 비밀번호 확인
+    password = request.data.get('password')
+    passwordConfirm = request.data.get('passwordConfirm')
+    # print(request.data)
+    if password != passwordConfirm:
+        return Response({'error': '비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
-    for i in range(1, 4):
-        URL = 'https://api.themoviedb.org/3/movie/popular'
-        params = {'language': 'ko-KR', 'page': i}
-        headers = {'accept': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGIzMmNlYzFjMTM0NGNjMzlmZWQ1N2MxZTI5MGFiNCIsInN1YiI6IjYzZDMxYmE2MDMxYTFkMDA4M2EwOTFlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lor3ESAInBSsko8e9uaQusOBVJ_DOxZY6gAfUAfWPM4'}
-    
-        res = requests.get(URL, params=params, headers=headers).json()
-        print(res['results'])
-        print('---------------------------------------------------------------------')
+    serializer = UserSerializer(data=request.data)
+    # print(serializer)
+    # 유효성 검사
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        # 비밀번호 hash 해서 저장
+        user.set_password(request.data.get('password'))
+        user.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def login(request):
+    password = request.data.get('password')
+    username = request.data.get('username')
+    # print(password, username)
+    users = get_object_or_404(User, username = username)
+    serializer = UserSerializer(users)
+    # print(serializer.data)
+    # return Response(serializer.data['id'], status = 200)
+
+    encoded = jwt.encode({"id": serializer.data['id']}, "JWT", algorithm="HS256")
+    data = {
+        "accessToken" : encoded
+    }
+    return Response(data, status = 200)
