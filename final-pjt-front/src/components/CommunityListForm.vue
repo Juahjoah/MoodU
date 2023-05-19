e6072672a1691137c4219c318d4a6ff7fe4d6e7e
Juahjoah <07juah21@gmail.com>
2023-05-19 16:59:16 +0900

    feat:Community-Component 연결

--- final-pjt-front/src/components/CommunityListForm.vue
+++ final-pjt-front/src/components/CommunityListForm.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="communityListFrom"></div>
+  <div class="CommunityListFrom">
+    <CommunityListItem />
+  </div>
 </template>
 
 <script>
+import CommunityListItem from "@/components/CommunityListItem.vue";
+
 export default {
   name: "CommunityListForm",
+  components: {
+    CommunityListItem,
+  },
 };
 </script>
 
---
+++ final-pjt-front/src/components/CommunityListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="CommunityItem">
+    <!-- <p>제목: {{ communityarticle.title }}</p> -->
+    <p>이거 굴러가나요</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommunityListItem",
+  data() {
+    return {};
+  },
+  // props: {
+  //   communityarticle: Object,
+  // },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
