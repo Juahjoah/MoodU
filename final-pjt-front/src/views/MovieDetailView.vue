61172340ee314f3f0db29635fc9edf00c344d78e
Juahjoah <07juah21@gmail.com>
2023-05-19 10:35:21 +0900

    design: 화면 배치 구성

--- final-pjt-front/src/views/MovieDetailView.vue
+++ final-pjt-front/src/views/MovieDetailView.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="moviedetail">
     <p>{{ movie.title }}</p>
+    <p>개봉일: {{ movie.release_date }}</p>
+    <br />
+    <img :src="getPoster" />
+    <hr />
     <p>{{ movie.overview }}</p>
-    <p>{{ movie.release_date }}</p>
+    <p>인기도: {{ movie.popularity }}</p>
+    <p>평균 점수: {{ movie.vote_average }}</p>
+    <p>투표 수: {{ movie.vote_count }}</p>
+    <hr />
+    <p>좋아요 누른 사용자 : {{ movie.like_users }}</p>
+    <section class="user_comment">
+      <p class="displaytext">여러분은 영화를 어떻게 보셨나요?</p>
+      <p>{{ movie.comment_set }}</p>
+    </section>
   </div>
 </template>
 
@@ -36,8 +48,17 @@ export default {
         });
     },
   },
+  computed: {
+    getPoster() {
+      return `https://image.tmdb.org/t/p/original/${this.movie.poster_path}`;
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+img {
+  float: right;
+  width: 15em;
+}
 </style>
