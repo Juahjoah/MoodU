deedbef538be2bb31dc7a1b11b82236d1a85b4bc
Juahjoah <07juah21@gmail.com>
2023-05-19 10:02:24 +0900

    [feat]: MovieDetail 기본 틀 작성

--- final-pjt-front/src/views/MovieDetailView.vue
+++ final-pjt-front/src/views/MovieDetailView.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div class="moviedetail"></div>
 </template>
 
 <script>
 export default {
   name: "MovieDetailView",
+  data() {
+    return {
+      movies: null,
+    };
+  },
+  created() {
+    this.getMovies();
+  },
+  methods: {
+    getMovies() {
+      this.$store.dispatch("getMovies");
+      this.movies = this.$store.state.movies;
+    },
+  },
 };
 </script>
 
