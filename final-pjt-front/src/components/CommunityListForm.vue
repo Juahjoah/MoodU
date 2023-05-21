29dbf19d9d0cda633dd9dcc98fadfe7cca8f4ec7
Juahjoah <07juah21@gmail.com>
2023-05-22 00:11:30 +0900

    feat: main화면 출력 수정 ing

--- final-pjt-front/src/components/CommunityListForm.vue
+++ final-pjt-front/src/components/CommunityListForm.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="CommunityListForm">
     <div class="communitylist" v-for="(community, idx) in communities" :key="idx">
-        <p>{{community}}</p>
-        <p> {{community.id}} </p>
-        <p> {{community.title}} </p>
-        <p> {{community.content}} </p>
+        <!-- <p>{{community}}</p> -->
+        <p> {{community.id}} {{community.title}} {{community.content}} </p>
     </div>
   </div>
 </template>
--- final-pjt-front/src/components/MovieCard.vue
+++ final-pjt-front/src/components/MovieCard.vue
@@ -1,21 +1,53 @@
 <template>
   <div class="card">
     <!-- <h1>MovieCard</h1> -->
-    <div class="each-movie-card">
+    <div class="each-movie-card"
+    data-glide-el="track"
+    ref="slider"
+    type="carousel"
+    :breakpoints="{3000: {perView: 7}, 1100: {perView: 5}, 600: {perView: 3}}"
+    >
+    <!-- <div class="each-movie-card"
+    type="carousel"
+    perView="6"
+    data-glide-el="track"
+    ref="slider"
+    :breakpoints="{3000: {perView: 7}, 1100: {perView: 5}, 600: {perView: 3}}"
+    > -->
       <router-link :to="{ name: 'MovieDetail', params: { id: movie.id } }">
         <img class="card-img-top" :src="getPoster" alt="Poster image" />
         <p>{{ movie.title }}</p>
         <p>{{ movie.overview }}</p>
+        <!-- <vue-glide 
+        class="glide__track"
+        data-glide-el="track"
+        ref="slider"
+        type="carousel"
+        :breakpoints="{3000: {perView: 7}, 1100: {perView: 5}, 600: {perView: 3}}"
+      >
+      <p>{{ movie.title }}</p> -->
+        <!-- <vue-glide-slide
+          v-for="(movie, idx) in movie"
+          :key="idx">
+          
+        </vue-glide-slide> -->
+      <!-- </vue-glide> -->
       </router-link>
     </div>
   </div>
 </template>
 
 <script>
+import { Glide, GlideSlide } from 'vue-glide-js'
+
 export default {
   name: "MovieCard",
   props: {
     movie: Object,
+  },
+  components:{
+    [Glide.name]: Glide,
+    [GlideSlide.name]: GlideSlide
   },
   computed: {
     getPoster() {
@@ -40,4 +72,15 @@ li {
 a {
   color: #42b983;
 }
+
+.card {
+  width: 13rem;
+  height: auto;
+}
+.card img {
+  width: 13rem;
+  height: auto;
+}
+
+
 </style>
