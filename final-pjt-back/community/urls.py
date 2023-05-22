b886980889464dff7f6f8a5eeee093cd02d556c7
yeg7481 <yeg7481@icloud.com>
2023-05-22 11:00:50 +0900

    fix : 모든 에러 수정

--- final-pjt-back/community/urls.py
+++ final-pjt-back/community/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'community'
 urlpatterns = [
     path('', views.community_list),
-    path('create/', views.community_create)
+    path('create/', views.community_create),
 ]
