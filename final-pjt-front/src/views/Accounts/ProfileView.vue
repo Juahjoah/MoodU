67b63dfae58ec8ff74f74873b049f094f13fb0e6
Juahjoah <07juah21@gmail.com>
2023-05-22 22:32:01 +0900

    feat: profileview create

---
+++ final-pjt-front/src/views/Accounts/ProfileView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="profile">
+      <h1>My profile</h1>
+      <section>
+
+      </section>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ProfileView",
+  };
+  </script>
+  
+  <style>
+  </style>
