99c6fa593543e4695fb7c90b4db3adfc7346f8a3
yeg7481 <yeg7481@icloud.com>
2023-05-22 14:35:21 +0900

    chore: 수정 기능 url 분리

--- final-pjt-back/community/urls.py
+++ final-pjt-back/community/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.community_list),
     path('create/', views.community_create),
     path('<int:review_pk>/', views.community_detail),
+    path('<int:review_pk>/update/', views.community_update)
 ]
