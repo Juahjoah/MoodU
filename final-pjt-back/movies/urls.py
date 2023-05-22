505ee855cae84bdad6455713fc202d3e84d26bc0
yeg7481 <yeg7481@icloud.com>
2023-05-22 11:26:59 +0900

    feat : 추천 url 생성

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -5,5 +5,6 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.index),
     path('<int:movie_pk>/', views.detail),
-    path('<int:movie_pk>/comments/create/', views.create_comment)
+    path('<int:movie_pk>/comments/create/', views.create_comment),
+    path('recommended/<str:emtion>/', views.recommend_movie),
 ]
