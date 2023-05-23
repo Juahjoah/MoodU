854746d5de2b43092ef0181ffbe2a2ccb58e154d
better-gyeom <better.gyeom@gmail.com>
2023-05-23 22:01:47 +0900

    chore: follower 추가

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -4,9 +4,15 @@ from movies.serializers import MovieSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     like_movies = MovieSerializer(many=True, read_only=True)
- 
+    
+    class followerSerializer(serializers.ModelSerializer):
+        
+        class Meta:
+            model = get_user_model()
+            fields = ('id', 'username', 'last_name',)
+    followers = followerSerializer(read_only = True, many=True)
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'last_name', 'email', 'followings', 'like_movies')
+        fields = ('id', 'username', 'last_name', 'email', 'followings', 'followers', 'like_movies')
         # fields = '__all__'
         read_only_fields = ('followings', 'like_movies')
