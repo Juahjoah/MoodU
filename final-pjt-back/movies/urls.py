1186f8a70f5c5753e7b2505cf20183e6d6253f7f
yeg7481 <yeg7481@icloud.com>
2023-05-23 15:49:52 +0900

    feat : 댓글 수정 삭제 로직 생성

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:movie_pk>/', views.detail),
     path('<int:movie_pk>/comments/create/', views.create_comment),
     path('<int:movie_pk>/comments/', views.comment),
+    path('<int:movie_pk>/comments/<int:comment_pk>/', views.comments_del_up),
     path('recommended/<str:emotion>/', views.recommend_movie),
     path('<int:movie_pk>/like/', views.like_movie),
     path('<int:movie_pk>/like/count/', views.like_movie_count),
