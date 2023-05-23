7d7c3f653e52f02a4e67f7f250fbd18d087915fb
yeg7481 <yeg7481@icloud.com>
2023-05-23 10:05:59 +0900

    feat: 추천 뷰 컴포넌트 생성

---
+++ final-pjt-front/src/views/Movie/RecommendView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>모듀님의 기분에 따른 영화를 추천해 드릴게요!</h1>
+        <div>
+            <span @click="emotionSelect('happy')">행복해요</span> |
+            <span @click="emotionSelect('sad')">슬퍼요</span> |
+            <span @click="emotionSelect('soso')">그저 그래요</span> |
+            <span @click="emotionSelect('joy')">신나요</span> |
+            <span @click="emotionSelect('angry')">화나요</span> |
+        </div>
+        <RecommendMovie :emo='emo'/>
+    </div>
+</template>
+
+<script>
+import RecommendMovie from '@/components/RecommendMovie'
+export default {
+    data() {
+        return {
+            emo: null,
+        }
+    },
+    methods: {
+        emotionSelect(e) {
+            console.log(e)
+            this.emo = e
+       }
+    },
+    components: {
+        RecommendMovie,
+    },
+}
+</script>
+
+<style>
+
+</style>
