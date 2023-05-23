e588ca2e0359ea2759648556ebfe375eb238dfdc
better-gyeom <better.gyeom@gmail.com>
2023-05-23 20:44:48 +0900

    chore: like_movies에 read_only추가로 에러 수정함

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -3,10 +3,10 @@ from django.contrib.auth import get_user_model
 from movies.serializers import MovieSerializer
 
 class UserSerializer(serializers.ModelSerializer):
-    like_movies = MovieSerializer(many=True)
+    like_movies = MovieSerializer(many=True, read_only=True)
  
     class Meta:
         model = get_user_model()
         fields = ('id', 'username', 'last_name', 'email', 'followings', 'like_movies')
         # fields = '__all__'
-        read_only_fields = ('followings',)
+        read_only_fields = ('followings', 'like_movies')
