e0ce1f90bdd5a11d62c5d9f06c10a9ed9f15c656
yeg7481 <yeg7481@icloud.com>
2023-05-23 13:08:08 +0900

    feat : follow 로직 구현

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
 class User(AbstractUser):
-    pass
+
+    # follow기능에 필요한 M:N 중개모델, 정참조 : followings / 역참조 : followers
+    followings = models.ManyToManyField(
+        'self', symmetrical=False, related_name='followers')
