a347da317698f2623b6ccb583fed0e2382442717
better-gyeom <better.gyeom@gmail.com>
2023-05-23 22:01:17 +0900

    feat: profile 화면에 구현

--- final-pjt-front/src/views/Accounts/ProfileView.vue
+++ final-pjt-front/src/views/Accounts/ProfileView.vue
@@ -1,15 +1,58 @@
 <template>
     <div class="profile">
       <h1>My profile</h1>
-      <section>
+      {{userData.username}}의 프로필
+      <div>
+        <p>
+          닉네임 : {{userData.last_name}}
+        </p>
+        <p>
+        이메일 : {{userData.email}}
 
-      </section>
+        </p>
+        <p>
+          팔로잉 : {{userData.followings}}
+          팔로워 : {{userData.followers}}
+        </p>
+      </div>
     </div>
   </template>
   
   <script>
+  import axios from 'axios'
+
+  const API_URL = 'http://127.0.0.1:8000'
+  const token = localStorage.getItem('jwt')
+
   export default {
     name: "ProfileView",
+    data() {
+      return {
+        userData: [],
+      }
+    },
+    methods: {
+      getUserData() {
+        const user = this.$store.state.user
+        axios({
+          method: 'post',
+          url: `${API_URL}/accounts/profile/${user}/`,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          }
+        })
+        .then((res)=> {
+          console.log(res.data)
+          this.userData = res.data
+        })
+        .catch((err)=> {
+          console.log(err)
+        })
+      }
+    },
+    created() {
+      this.getUserData()
+    }
   };
   </script>
   
