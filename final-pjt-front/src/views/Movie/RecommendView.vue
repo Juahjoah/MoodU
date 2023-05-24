fe8073e695af24d6b910f86271919d5c2ab56c33
Juahjoah <07juah21@gmail.com>
2023-05-24 09:39:34 +0900

    fix: 비로그인 유저 recommend 접근 불가

--- final-pjt-front/src/views/Movie/RecommendView.vue
+++ final-pjt-front/src/views/Movie/RecommendView.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <h3>모듀님의 기분에 따른 영화를 추천해 드릴게요!</h3>
-    <div>
-      <span @click.self.prevent="emotionSelect('happy')">행복해요</span> |
-      <span @click.self.prevent="emotionSelect('sad')">슬퍼요</span> |
-      <span @click.self.prevent="emotionSelect('soso')">그저 그래요</span> |
-      <span @click.self.prevent="emotionSelect('joy')">신나요</span> |
-      <span @click.self.prevent="emotionSelect('angry')">화나요</span> |
+    <h5>현재 기분을 골라주세요!</h5>
+    <div class="emobtn">
+      <span class="happy" @click.self.prevent="emotionSelect('happy')"
+        >행복해요</span
+      >
+      |
+      <span class="sad" @click.self.prevent="emotionSelect('sad')">슬퍼요</span>
+      |
+      <span class="soso" @click.self.prevent="emotionSelect('soso')"
+        >그저 그래요</span
+      >
+      |
+      <span class="joy" @click.self.prevent="emotionSelect('joy')">신나요</span>
+      |
+      <span class="angry" @click.self.prevent="emotionSelect('angry')"
+        >화나요</span
+      >
+      |
     </div>
     <h5>{{ emo }} 모듀님을 위한 영화</h5>
     <RecommendMovie
@@ -30,6 +42,9 @@ export default {
       emotionMovie: [],
     };
   },
+  components: {
+    RecommendMovie,
+  },
   methods: {
     emotionSelect(e) {
       // console.log(e);
@@ -46,19 +61,19 @@ export default {
           Authorization: `Bearer ${token}`,
         },
       })
-      .then((res) => {
-        // console.log(res);
-        this.emotionMovie = res.data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then((res) => {
+          // console.log(res);
+          this.emotionMovie = res.data;
+        })
+        .catch((err) => {
+          confirm("로그인한 콩알님들만 볼 수 있어요😣!");
+          this.$router.push({ name: "LoginView" });
+          console.log(err);
+        });
     },
-    
   },
-
-  components: {
-    RecommendMovie,
+  created() {
+    this.emotionSelect();
   },
 };
 </script>
