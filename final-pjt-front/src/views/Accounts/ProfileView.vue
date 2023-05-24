04a640632518bde836c45145a5254fd13c6504de
Juahjoah <07juah21@gmail.com>
2023-05-24 16:54:13 +0900

    design: profile following, follower 기능 구현

--- final-pjt-front/src/views/Accounts/ProfileView.vue
+++ final-pjt-front/src/views/Accounts/ProfileView.vue
@@ -4,48 +4,98 @@
       <h1>나의 프로필</h1>
       <div class="myinfo">
         <div class="basicinfo">
-          <i class="fal fa-user-circle"></i>
-          <p>아이디 : {{ userData.username }}</p>
-          <p>이메일 : {{ userData.email }}</p>
+          <i class="bi bi-person-circle"></i>
+          <div class="idemail">
+            <p>아이디 : {{ userData.username }}</p>
+            <p>이메일 : {{ userData.email }}</p>
+          </div>
         </div>
-        <p>팔로잉 : {{ userData.followings.length }}</p>
-        <li v-for="(following, index) in userData.followings" :key="index">
-          {{ following.username }}
-        </li>
-        <p>팔로워 : {{ userData.followers.length }}</p>
-        <li v-for="(follower, index) in userData.followers" :key="index">
-          {{ follower.username }}
-        </li>
-        <p>내가 좋아요 한 영화 개수 : {{ userData.like_movies.length }}</p>
-        <li v-for="(like, index) in userData.like_movies" :key="index">
-          {{ like.title }}
-        </li>
+        <div class="followbox">
+          <p>팔로잉 {{ userData.followings.length }}명</p>
+          <div
+            class="followingsbox"
+            v-for="(following, index) in userData.followings"
+            :key="index"
+          >
+            <div class="followings">
+              <i class="biuser bi-person-hearts"></i>
+              {{ following.username }}
+            </div>
+          </div>
+          <p>팔로워 {{ userData.followers.length }}명</p>
+          <div
+            class="followersbox"
+            v-for="(follower, index) in userData.followers"
+            :key="index"
+          >
+            <div class="followers">
+              <i class="biuser bi-person-heart"></i>
+              {{ follower.username }}
+            </div>
+          </div>
+        </div>
+        <div class="likemovie">
+          <p>내가 좋아요 한 영화 개수 : {{ userData.like_movies.length }}</p>
+          <li v-for="(like, index) in userData.like_movies" :key="index">
+            {{ like.title }}
+          </li>
+        </div>
       </div>
     </div>
     <div v-else>
+      <h1>{{ userData.username }}님의 프로필</h1>
       <div class="userinfo">
-        <h1>{{ userData.username }}님의 프로필</h1>
-        <p>아이디 : {{ userData.username }}</p>
-        <p>이메일 : {{ userData.email }}</p>
-        <p>팔로잉 : {{ userData.followings.length }}</p>
-        <li v-for="(following, index) in userData.followings" :key="index">
-          {{ following.username }}
-        </li>
-        <p>팔로워 : {{ userData.followers.length }}</p>
-        <li v-for="(follower, index) in userData.followers" :key="index">
-          {{ follower.username }}
-        </li>
-        <p>
-          {{ userData.username }}님이 좋아요 한 영화 개수 :
-          {{ userData.like_movies.length }}
-        </p>
-        <li v-for="(like, index) in userData.like_movies" :key="index">
-          {{ like.title }}
-        </li>
+        <div class="basicinfo">
+          <div class="userfollow">
+            <i class="bi bi-person-circle"></i>
+            <button
+              type="button"
+              class="followbtn"
+              @click.self.prevent="followingUser()"
+              data-text="팔로우"
+            >
+              {{ followMsg }}
+            </button>
+          </div>
+          <div class="idemail">
+            <p>아이디 : {{ userData.username }}</p>
+            <p>이메일 : {{ userData.email }}</p>
+          </div>
+        </div>
+        <div class="followbox">
+          <p>팔로잉 {{ userData.followings.length }}명</p>
 
-        <button @click="followingUser()">
-          {{ followMsg }}
-        </button>
+          <div
+            class="followingsbox"
+            v-for="(following, index) in userData.followings"
+            :key="index"
+          >
+            <div class="followings">
+              <i class="biuser bi-person-hearts"></i>
+              {{ following.username }}
+            </div>
+          </div>
+          <p>팔로워 {{ userData.followers.length }}명</p>
+          <div
+            class="followersbox"
+            v-for="(follower, index) in userData.followers"
+            :key="index"
+          >
+            <div class="followers">
+              <i class="biuser bi-person-heart"></i>
+              {{ follower.username }}
+            </div>
+          </div>
+        </div>
+        <div class="likemovie">
+          <p>
+            {{ userData.username }}님이 좋아요 한 영화
+            {{ userData.like_movies.length }} 개
+          </p>
+          <li v-for="(like, index) in userData.like_movies" :key="index">
+            {{ like.title }}
+          </li>
+        </div>
       </div>
     </div>
   </div>
@@ -117,5 +167,63 @@ export default {
 .myinfo {
   display: flex;
   flex-direction: column;
+  width: 40rem;
+  height: 80rem;
+  margin: 0px auto;
+}
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  width: 40rem;
+  height: 80rem;
+  margin: 0px auto;
+}
+.userfollow {
+  display: flex;
+  flex-direction: column;
+}
+.followbtn {
+  width: 15rem;
+}
+.followbtn::after {
+  content: attr(data-text);
+}
+
+.basicinfo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bi {
+  font-size: 15rem;
+  margin-right: 10rem;
+}
+
+.followbox {
+  text-align: left;
+}
+.followersbox {
+  display: inline-block;
+}
+.followers {
+  width: 7rem;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+}
+.followingsbox {
+  display: inline-block;
+}
+.followings {
+  width: 7rem;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+}
+.biuser {
+  margin-top: 1rem;
+  font-size: 3rem;
 }
 </style>
