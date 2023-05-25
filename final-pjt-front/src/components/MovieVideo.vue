b6aa275d16aa2df90507568bfb53a4f6bcfd980c
Juahjoah <07juah21@gmail.com>
2023-05-26 00:00:09 +0900

    design: youtube btn

--- final-pjt-front/src/components/MovieVideo.vue
+++ final-pjt-front/src/components/MovieVideo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="movievideo">
-    <button @click="getVideo()">유튜브 영상보기</button>
+    <button class="btn-3d clickbtn" @click="getVideo()">유튜브 영상보기</button>
     <div>
       <p v-if="videoMsg">{{ videoMsg }}</p>
       <p v-else>
@@ -66,7 +66,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .movievideo {
   margin-top: 4rem;
   right: 50%;
@@ -81,4 +81,45 @@ export default {
 .youtubemovie {
   margin: 0 auto;
 }
+
+.clickbtn {
+  margin: 0.7rem;
+  /* display: flex; */
+}
+
+.btn-3d::after {
+  content: attr(data-text);
+}
+.btn-3d {
+  font-family: "TheJamsil5Bold";
+  width: 13rem;
+  height: 2.3rem;
+  position: relative;
+  display: inline-block;
+  font-size: 13px;
+  margin: 20px 10px 10px;
+  border-radius: 6px;
+  transition: top 0.01s linear;
+  text-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
+}
+.btn-3d.clickbtn:hover {
+  background-color: #f51f1f;
+}
+
+.btn-3d:active {
+  top: 9px;
+}
+/* 3D button colors */
+.btn-3d.clickbtn {
+  background-color: #f56e6e;
+  box-shadow: 0 0 0 1px #f51f1f inset, 0 0 0 2px rgba(255, 255, 255, 0.15) inset,
+    0 8px 0 0 #A30808, 0 8px 0 1px rgba(0, 0, 0, 0.4),
+    0 8px 8px 1px rgba(0, 0, 0, 0.5);
+}
+.btn-3d.clickbtn:active {
+  box-shadow: 0 0 0 1px #f51f1f inset, 0 0 0 2px rgba(255, 255, 255, 0.15) inset,
+    0 0 0 1px rgba(0, 0, 0, 0.4);
+}
+
+
 </style>
