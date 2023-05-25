9e25e873d858b97f8ae3dd3a21ff4070c8a55967
Juahjoah <07juah21@gmail.com>
2023-05-25 15:00:59 +0900

    design: movie main error fixed

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -22,9 +22,9 @@
         <div class="boundary1">
           <router-link :to="{ name: 'MovieView' }">
             <img
-            class="logoimg"
-            src="./assets/logo_black_width.png"
-            alt="moodU_logo"
+              class="logoimg"
+              src="./assets/logo_black_width.png"
+              alt="moodU_logo"
             />
           </router-link>
           <router-link to="/">Movie</router-link>
@@ -37,12 +37,13 @@
               name: 'ProfileView',
               params: { username: $store.state.user },
             }"
-            @click="profile">my profile</router-link
+            @click="profile"
+            >my profile</router-link
           >
         </div>
       </section>
       <hr />
-      
+
       <!-- <header>
         <div class="hidiv" v-if="!$store.state.user">
           <h6 align="right">오늘도 모듀와 함께 행복해요.</h6>
@@ -70,68 +71,144 @@ export default {
       this.$store.dispatch("logout");
     },
     profile() {
-      this.$store.dispatch('profile')
-    }
+      this.$store.dispatch("profile");
+    },
   },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR&display=swap");
 </style>
 <style>
-html, body, div, span, applet, object, iframe, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
 }
 body {
-	line-height: 1;
+  line-height: 1;
 }
-ol, ul {
-	list-style: none;
+ol,
+ul {
+  list-style: none;
 }
-blockquote, q {
-	quotes: none;
+blockquote,
+q {
+  quotes: none;
 }
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
 }
 table {
-	border-collapse: collapse;
-	border-spacing: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 
 @font-face {
-    font-family: 'TheJamsil5Bold';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil5Bold.woff2') format('woff2');
-    font-weight: 700;
-    font-style: normal;
+  font-family: "TheJamsil5Bold";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil5Bold.woff2")
+    format("woff2");
+  font-weight: 700;
+  font-style: normal;
 }
 
 #app {
-  font-family: 'TheJamsil5Bold';
+  font-family: "TheJamsil5Bold";
   font-size: 20px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -142,21 +219,20 @@ table {
 nav {
   padding: 1rem;
   height: 1.5rem;
-
 }
 
 nav section {
   display: flex;
   justify-content: space-between;
   font-size: 20px;
-  align-items: end;
+  align-items: flex-end;
 }
 
 .boundary1 {
   width: 30rem;
   display: flex;
   justify-content: space-evenly;
-  align-items: end;
+  align-items: flex-end;
 }
 .boundary2 {
   width: 15rem;
@@ -181,7 +257,7 @@ nav a.router-link-exact-active {
 }
 
 header {
-  padding-top: 1.0rem;
+  padding-top: 1rem;
   padding-right: 1rem;
   height: 2rem;
   /* background-color: #ebe8e1 ; */
@@ -190,5 +266,4 @@ header {
 .hidiv {
   height: 3rem;
 }
-
 </style>
