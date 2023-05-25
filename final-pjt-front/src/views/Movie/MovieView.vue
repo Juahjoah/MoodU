56df842f08ee0d85f5899b802d9b241c3e4f34d4
Juahjoah <07juah21@gmail.com>
2023-05-25 10:27:32 +0900

    design: main banner

--- final-pjt-front/src/views/Movie/MovieView.vue
+++ final-pjt-front/src/views/Movie/MovieView.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="Movie">
     <div class="searchbox">
-      <i class="bi bi-search"> 
-        <input type="text" v-model="searchMovie" @keyup.enter="search()" /> </i>
+      <i class="bi bi-search">
+        <input type="text" v-model="searchMovie" @keyup.enter="search()" />
+      </i>
     </div>
-    <br><br><br><br>
+    <br /><br /><br /><br />
 
-    <section class="moviepic">
+    <section class="moviepic"></section>
 
-    </section>
-    
-    <br><br><br><br>
+    <br /><br /><br /><br />
 
     <div id="banner">
-    <router-link      
-    :to="{ name: 'RecommendView' }"
-    > 
-          <p class="ch"> 오늘 어떤 영화볼지 고민인가요?</p>
-          <!-- <img class="tistory" src="images/ch_logo.png"> -->
-          <p class="txt">오늘의 기분을 고르시면,</p>
-          <p class="txt2">모듀가 직접 영화를 골라드려요!</p>
-          <p class="logo"><i class="fab fa-chrome"></i><i class="fab fa-internet-explorer"></i><i class="fab fa-firefox"></i><i class="fab fa-opera"></i><i class="fab fa-safari"></i></p>
-          <p class="device"><i class="fas fa-desktop"></i><i class="fas fa-mobile-alt"></i></p>
-        </router-link>
-      </div>
-  
+      <router-link :to="{ name: 'RecommendView' }">
+        <p class="ch">모듀 여기를 주목해주세요!</p>
+        <p class="focus">오늘 어떤 영화볼지 고민되시는 분들!</p>
+        <i class="handfocus bi bi-hand-index-fill"></i>
+        <p class="txt">오늘의 기분을 고르시면,</p>
+        <p class="txt2">모듀가 직접 영화를 골라드려요!</p>
+        <p class="logo">
+          <!-- 아래에서 날라옴 -->
+        </p>
+        <p class="device">
+          <!-- 제자리에서 사라지기 -->
+          <i class="bi bi-film"></i>
+        </p>
+        <img
+          class="bannerfooter"
+          src="@/assets/logo_black_width.png"
+          alt="moodU_logo"
+        />
+      </router-link>
+    </div>
 
     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
     <h1>Movie</h1>
@@ -74,11 +81,10 @@ export default {
 
 <style scoped>
 * {
-  font-family: 'TheJamsil5Bold';
+  font-family: "TheJamsil5Bold";
 }
 .Movie {
-
-  font-family: 'TheJamsil5Bold';
+  font-family: "TheJamsil5Bold";
 }
 input {
   width: 20rem;
@@ -109,197 +115,119 @@ i {
   width: 80%;
 }
 
-
-
 a {
-
-text-decoration: none;
-
-color: black;
-
+  text-decoration: none;
+  color: black;
 }
-
-
 
 #banner {
-display: inline-flex;
-position: relative;
-
-overflow: hidden;
-
-width: 700px;
-
-height: 250px;
-
-border: 1px solid #ccc;
-
-background: #E3DAD5 
-
+  display: inline-flex;
+  position: relative;
+  overflow: hidden;
+  width: 46.25rem;
+  height: 10rem;
+  border: 1px solid #ccc;
+  background: hsl(20, 100%, 98%);
 }
 
-
-
+.focus {
+  font-size: 30px;
+  position: absolute;
+  left: 70px;
+  top: 15px;
+  transition: all 0.5s 0.2s;
+}
+#banner:hover .focus {
+  top: 800px;
+}
+.handfocus {
+  font-size: 30px;
+  position: absolute;
+  left: 530px;
+  top: 45px;
+  transition: all 0.5s 0.2s;
+}
+#banner:hover .handfocus {
+  top: 800px;
+}
 .ch {
-
-position: absolute;
-
-left: 290px;
-
-top: 10px;
-
-transition: all 0.5s;
-
+  position: absolute;
+  left: 70px;
+  top: 70px;
+  transition: all 0.5s;
 }
-
-
 
 #banner:hover .ch {
-
-left: 520px;
-
+  top: 800px;
 }
-
-
-
-.tistory {
-
-position: absolute;
-
-left: 15px;
-
-top: -90px;
-
-transition: all 0.5s 0.2s;
-
-}
-
-
-
-#banner:hover .tistory {
-
-top: 15px;
-
-}
-
-
 
 .txt {
-
-display: block;
-
-position: absolute;
-
-left: 20px;
-
-top: 110px;
-
-font-size: 30px;
-
-font-weight: bold;
-
-opacity: 0;
-
-transform: scale(2, 2);
-
-transition: all 0.25s linear 0.2s;
-
+  display: block;
+  position: absolute;
+  left: 70px;
+  top: 15px;
+  font-size: 30px;
+  font-weight: bold;
+  opacity: 0;
+  transform: scale(2, 2);
+  transition: all 0.25s linear 0.2s;
 }
-
-
-
 #banner:hover .txt {
-
-opacity: 1;
-
-transform: scale(1, 1);
-
+  opacity: 1;
+  transform: scale(1, 1);
 }
-
-
 
 .txt2 {
-
-display: block;
-
-position: absolute;
-
-left: -300px;
-
-bottom: 20px;
-
-font-size: 30px;
-
-font-weight: bold;
-
-color: #555;
-
-transition: all 0.5s 0.3s;
-
+  display: block;
+  position: absolute;
+  left: -400px;
+  bottom: 20px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #555;
+  transition: all 0.5s 0.3s;
 }
-
-
 
 #banner:hover .txt2 {
-
-left: 18px;
-
+  left: 70px;
 }
-
-
 
 .logo {
-
-display: block;
-
-position: absolute;
-
-left: 270px;
-
-bottom: -50px;
-
-font-size: 20px;
-
-color: #888;
-
-transition: all 0.5s 0.3s;
-
+  display: block;
+  position: absolute;
+  left: 70px;
+  bottom: -50px;
+  font-size: 20px;
+  color: #888;
+  transition: all 0.5s 0.3s;
 }
-
-
 
 #banner:hover .logo {
-
-bottom: 20px;
-
+  bottom: 20px;
 }
-
-
 
 .device {
-
-display: block;
-
-position: absolute;
-
-right: 180px;
-
-bottom: 10px;
-
-font-size: 55px;
-
-color: #007AAE;
-
-opacity: 0;
-
-transition: opacity 0.5s linear 0.2s;
-
+  display: block;
+  position: absolute;
+  left: 500px;
+  /* top: 15px; */
+  bottom: 10px;
+  font-size: 70px;
+  /* color: #007aae; */
+  color: black;
+  opacity: 0;
+  transition: opacity 0.5s linear 0.2s;
 }
 
-
-
 #banner:hover .device {
-
-opacity: 1;
-
+  opacity: 1;
+}
+.bannerfooter {
+  display: block;
+  position: absolute;
+  bottom: 1px;
+  right: 1px;
+  width: 4rem;
+  height: auto;
 }
 </style>
